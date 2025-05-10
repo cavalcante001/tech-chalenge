53dd92fe84854d8d6279acc5841ab875e3a5adc4
Shayna Bauer <shayna.bauer@olxbr.com>
2025-05-10 17:22:36 -0300

    fix: add single quote to categories uuid

--- migrations/1746405856788-Category.ts
+++ migrations/1746405856788-Category.ts
@@ -14,10 +14,10 @@ export class Category1746405856788 implements MigrationInterface {
 
     await queryRunner.query(`
             INSERT INTO "categories" ("id", "name") VALUES
-            (b477f4bc-373a-4e8a-ac71-11523b821a0e, 'Lanche'),
-            (0a648c21-f468-47e1-98b4-10ce0d34da52, 'Acompanhamento'),
-            (dd971d31-e2c9-438e-8882-8e7b793be7d1, 'Bebida'),
-            (1bb9953d-1f80-4888-b402-df7dde0c2581, 'Sobremesa')
+            ('b477f4bc-373a-4e8a-ac71-11523b821a0e', 'Lanche'),
+            ('0a648c21-f468-47e1-98b4-10ce0d34da52', 'Acompanhamento'),
+            ('dd971d31-e2c9-438e-8882-8e7b793be7d1', 'Bebida'),
+            ('1bb9953d-1f80-4888-b402-df7dde0c2581', 'Sobremesa')
         `);
   }
 
