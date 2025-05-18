4369e4765878c9fe47d220aabd9e87f95aafaccd
Paulo Gomes <paulocavalcante001@outlook.com>
2025-05-18 16:37:45 -0300

    fix: ajustes

--- src/orders/application/queries/get-orders.query-handler.ts
+++ src/orders/application/queries/get-orders.query-handler.ts
@@ -6,9 +6,9 @@ import { OrderReadModel } from 'src/orders/domain/read-models/order.read-model';
 
 @QueryHandler(GetOrdersQuery)
 export class GetOrdersQueryHandler implements IQueryHandler<GetOrdersQuery> {
-  constructor(private readonly findOrdersRepository: OrderReadyRepository) {}
+  constructor(private readonly orderRepository: OrderReadyRepository) {}
 
   async execute(): Promise<OrderReadModel[]> {
-    return this.findOrdersRepository.findAll();
+    return this.orderRepository.findAll();
   }
 } 
